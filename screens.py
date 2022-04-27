51021ad28c0a52c2e0e231e7ede4870ef1dd2af3
biscuitrescue <karttikeyasinha11@gmail.com>
2022-04-27 22:51:14 +0530

    updated

--- screens.py
+++ screens.py
@@ -1,40 +1,38 @@
 import os
-from libqtile.config import Screen
-from libqtile import bar, widget
-from libqtile import qtile
+from libqtile.config import Screen 
+from libqtile import layout, bar, widget, hook
 
-colours = [
-    ["#D8DEE9"],        # Colour 0
-    ["#282a36"],        # Colour 1
-    ["#F2779C"],        # Colour 2
-    ["#50fa7b"],        # Colour 3
-    ["#f1fa8c"],        # Colour 4
+colours =  [
+    ["#D8DEE9"],      # Colour 0
+    ["#1e1e2e"],        # Colour 1
+    ["#f28fad"],        # Colour 2
+    ["#abe9b3"],        # Colour 3
+    ["#fae3b0"],        # Colour 4
     ["#d6acff"],        # Colour 5
-    ["#ff79c6"],        # Colour 6
-    ["#8be9fd"],        # Colour 7
-    ["#ff6e6e"],        # Colour 8
-    ["#a4ffff"],        # Colour 9
-    ["#ff5555"]         # Colour 10
-]
+    ["#f5c2e7"],        # Colour 6
+    ["#89DCEB"],        # Colour 7
+    ["#C9CBFF"],        # Colour 8
+    ["#b5e8e0"],        # Colour 9
+    ["#F2779C"]]        # Colour 10
 
-xx = 16
-xf = "operatormono nerd font medium"
-default = [
+xx=17
+xf="ubuntumono nerd font bold"
+default=[
     widget.GroupBox(
-        font="operator mono medium",
-        fontsize=17,
+        font="ubuntumono nerd font bold",
+        fontsize=16,
         background=colours[1],
-        margin_y=3,
+        margin_y=4,
         margin_x=5,
         padding_y=3,
         padding_x=2,
         borderwidth=8,
-        inactive=colours[5],
+        inactive=colours[8],
         active=colours[3],
         rounded=True,
         highlight_color=colours[4],
         highlight_method="block",
-        this_current_screen_border=colours[8],
+        this_current_screen_border=colours[10],
         block_highlight_text_color=colours[1],
     ),
     widget.Sep(
@@ -61,7 +59,7 @@ default = [
     ),
     widget.CPU(
         foreground=colours[9],
-        format=' {load_percent}%',
+        format='  {load_percent}%',
         font=xf,
         fontsize=xx,
     ),
@@ -76,7 +74,7 @@ default = [
         foreground=colours[4],
         measure_mem='G',
         measure_swap='G',
-        format='{MemUsed: .2f} GB',
+        format=' {MemUsed: .2f} GB',
     ),
     widget.TextBox(
         foreground=colours[6],
@@ -89,16 +87,16 @@ default = [
         fontsize=xx,
         foreground=colours[6],
         measure_swap='G',
-        format='{SwapUsed: .2f} GB',
+        format=' {SwapUsed: .2f} GB',
     ),
     widget.TextBox(
-        foreground=colours[7],
+        foreground=colours[3],
         text=" | ",
         font=xf,
     ),
     widget.PulseVolume(
         mouse_callbacks={'Button3': lambda: qtile.cmd_spawn("pavucontrol")},
-        foreground=colours[7],
+        foreground=colours[3],
         update_interval=0.001,
         font=xf,
         fontsize=xx,
@@ -110,7 +108,7 @@ default = [
     ),
     widget.Clock(
         foreground=colours[8],
-        format=' %d %b, %a',
+        format='  %d %b, %a',
         font=xf,
         fontsize=xx,
     ),
@@ -123,15 +121,15 @@ default = [
         foreground=colours[5],
         font=xf,
         fontsize=xx,
-        format=' %I:%M %p',
+        format='  %I:%M %p',
     ),
     widget.TextBox(
-        foreground=colours[3],
+        foreground=colours[7],
         text=" | ",
         font=xf,
     ),
 ]
-if len(os.listdir("/sys/class/power_supply")) == 0:
+if len(os.listdir("/sys/class/power_supply"))==0:
     default.append(
         widget.CapsNumLockIndicator(
             fontsize=xx,
@@ -151,12 +149,12 @@ else:
                 low_foreground=colours[1],
                 update_interval=1,
                 charge_char='',
-                discharge_char='',
-                foreground=colours[3],
-                format='{char} {percent:2.0%}',
+                discharge_char=' ',
+                foreground=colours[7],
+                format='{char}  {percent:2.0%}',
             ),
             widget.TextBox(
-                foreground=colours[3],
+                foreground=colours[7],
                 text=" |",
                 font=xf,
             ),
@@ -165,13 +163,13 @@ else:
 
 screens = [
     Screen(
-        top=bar.Bar(
-            default,
-            32,
-            background=colours[1],
-            foreground=colours[1],
-            # opacity=0.9,
-            margin=[10, 80, 8, 80],
-        ),
+    top=bar.Bar(
+        default,
+        34,
+        background=colours[1],
+        foreground=colours[1],
+        opacity=0.95,
+        margin=[8,60,12,60],
+    ),
     ),
 ]
