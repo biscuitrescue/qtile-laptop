dd9015ae01461ef47dac84dc7acd38db6c463611
biscuitrescue <karttikeyasinha11@gmail.com>
2022-05-24 10:56:57 +0530

    updated

--- config.py
+++ config.py
@@ -46,6 +46,7 @@ floating_layout = layout.Floating(
         Match(wm_class='maketag'),  # gitk
         Match(wm_class='ssh-askpass'),  # ssh-askpass
         Match(wm_class='Tk'),  # ssh-askpass
+        # Match(wm_class=endswith(".py")),
         Match(wm_class='Signal Beta'),  # ssh-askpass
         Match(title='branchdialog'),  # gitk
         Match(title='pinentry'),  # GPG key password entry
